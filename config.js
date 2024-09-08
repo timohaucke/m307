1b7ee63b1610f3bbb9b61156ef450e6446a07757
Lukas Diener <junglekiddy@gmail.com>
2024-09-08 07:42:32 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import pg from "pg";
+const { Pool } = pg;
+import cookieParser from "cookie-parser";
+import multer from "multer";
+const upload = multer({ dest: "public/uploads/" });
+import sessions from "express-session";
+
+export function createApp(dbconfig) {
+  const app = express();
+
+  const pool = new Pool(dbconfig);
+
+  app.engine("handlebars", engine());
+  app.set("view engine", "handlebars");
+  app.set("views", "./views");
+
+  app.use(express.static("public"));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+
+  app.use(
+    sessions({
+      secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+      saveUninitialized: true,
+      cookie: { maxAge: 86400000, secure: false },
+      resave: false,
+    })
+  );
+
+  app.locals.pool = pool;
+
+  return app;
+}
+
+export { upload };
---
+++ index.js
@@ -0,0 +1,23 @@
+import { createApp } from "./config.js";
+
+const app = createApp({
+  user: "autumn_star_7622",
+  host: "168.119.168.41",
+  database: "demo",
+  password: "uaioysdfjoysfdf",
+  port: 18324,
+});
+
+/* Startseite */
+app.get("/", async function (req, res) {
+  res.render("start", {});
+});
+
+app.get("/impressum", async function (req, res) {
+  res.render("impressum", {});
+});
+
+/* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
+app.listen(3010, () => {
+  console.log(`Example app listening at http://localhost:3010`);
+});
